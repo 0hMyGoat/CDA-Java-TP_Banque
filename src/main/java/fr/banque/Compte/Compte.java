8f8e550cb1c45979b29715286216b37a4cb559c1
Oh_My_Goat <daftouney@gmail.com>
2022-09-26 15:12:16 +0200

    feat: fin exercice 6A - Création Compte :sparkles:

--- src/main/java/fr/banque/Compte/Compte.java
+++ src/main/java/fr/banque/Compte/Compte.java
@@ -1,5 +1,60 @@
 package fr.banque.Compte;
 
 public class Compte {
+
+    int numero;
+    double solde;
+
+    public Compte(){
+        this.setNumero(0);
+        this.setSolde(0);
+    }
+
+    public Compte(int unNumero, double soldeInitial) {
+        this.setNumero(unNumero);
+        this.setSolde(soldeInitial);
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    private void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+
+    private void setSolde(double solde) {
+        this.solde = solde;
+    }
+
+    public void ajouter(double unMontant) {
+        this.solde += unMontant;
+    }
+
+    public void retirer(double unMontant) {
+        this.setSolde(solde -= unMontant);
+    }
+
+    public String toString() {
+        return "Compte [numero=" + numero + ", solde=" + solde + "]";
+    }
+
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (obj.getClass() != this.getClass()) {
+            return false;
+        }
+        Compte compte = (Compte) obj;
+        return (this.numero == compte.numero && this.solde == compte.solde);
+    }
     
 }
